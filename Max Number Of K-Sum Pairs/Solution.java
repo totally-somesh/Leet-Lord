a25d6abd7a86ac9f2bf53c832a53d33f61f61ebd
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-09-03 20:23:35 +0530

    Question No. : 13 : Max Number Of K-Sum Pairs

---
+++ Max Number Of K-Sum Pairs/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+
+        Arrays.sort(nums);
+
+        int operations = 0;
+        int i = 0;
+        int j = nums.length-1;
+
+        while(i < j){
+
+            if(nums[i] + nums[j] == k){
+
+                operations++; i++; j--;
+            }
+            else if(nums[i] + nums[j] < k){
+                i++;
+            }else{
+                j--;
+            }
+        }     
+
+            return operations; 
+        }
+    }
