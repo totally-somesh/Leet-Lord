29ab22b72ec32bfa04b3b2fc7443f7ec9ca2961b
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-09-04 19:56:08 +0530

    Method Name Changed

--- Kth Largest Element In An Array/Solution.java
+++ Kth Largest Element In An Array/Solution.java
@@ -1,48 +1,50 @@
 class Solution {
-    
-    public int partition(int [] arr, int low, int high){
+	
+	public int partition(int [] arr, int low, int high){
 
-        int pivot = arr[high];
-        int pivotloc = low;
+		int pivot = arr[high];
+		int pivotloc = low;
+		
+		for(int i=low; i<=high; i++){
+			
+			if(arr[i] > pivot){
 
-        for(int i=low; i<= high; i++){
-            
-            if(arr[i] > pivot){
+				int temp = arr[i];
+				arr[i] = arr[pivotloc];
+				arr[pivotloc] = temp;
+				pivotloc++;				
+			}		
+		}
+		
+		int temp = arr[high];
+		arr[high] = arr[pivotloc];
+		arr[pivotloc] = temp;
+		
+		return pivotloc;		
+	}
+	
+	public int kthlargest(int [] arr, int low, int high, int k){
+		
+		int partition = partition(arr, low, high);
+		
+		if(partition == k-1){
+			
+			return arr[partition];			
+		}
+		else if(partition < k-1){
 
-                int temp = arr[i];
-                arr[i] = arr[pivotloc];
-                arr[pivotloc] = temp;
-                pivotloc++;
-            }
-        }
-
-        int temp = arr[high];
-        arr[high] = arr[pivotloc];
-        arr[pivotloc] = temp;
-
-        return pivotloc;
-    }
-
-    public int kthsmallest(int [] arr, int low, int high, int k){
-
-        int partition = partition(arr, low, high);
-
-        if(partition == k-1){
-
-            return arr[partition];
-        } 
-        else if(partition < k-1){
-
-            return kthsmallest(arr, partition+1, high, k);
-        }
-        else{
-
-            return kthsmallest(arr, low, partition-1, k);
-        }
-    }
-    
-    public int findKthLargest(int[] nums, int k) {
-
-        return kthsmallest(nums, 0, nums.length-1, k);
-    }
+			return kthlargest(arr, partition+1, high, k);			
+		}
+		else{
+			
+			return kthlargest(arr, low, partition-1, k);
+		}		
+	}
+	
+	
+	int getKthLargestElement(int[] list, int k) {
+	    				
+		return kthlargest(list, 0, list.length-1, k);		
+	}
+	
 }
